49028433da09ae0cd2fcb530a8a47edfe38f2a82
7Avi7 <101573896+7Avi7@users.noreply.github.com>
2024-05-30 22:55:19 +0600

    1st commit

---
+++ Frontend/amazon_clone/lib/common/widgets/bottom_bar.dart
@@ -0,0 +1,143 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/account/screens/account_screen.dart';
+import 'package:amazon_clone/features/cart/screens/cart_screen.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class BottomBar extends StatefulWidget {
+  static const String routeName = '/actual-home';
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  int _page = 0;
+  double bottomBarWidth = 42;
+  double bottomBarBorderWidth = 5;
+
+  List<Widget> pages = [
+    const HomeScreen(),
+    const AccountScreen(),
+    const CartScreen(),
+  ];
+
+  void updatePage(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final userCartLen = context.watch<UserProvider>().user.cart.length;
+
+    return Scaffold(
+      body: pages[_page],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _page,
+        selectedItemColor: GlobalVariables.selectedNavBarColor,
+        unselectedItemColor: GlobalVariables.unselectedNavBarColor,
+        backgroundColor: GlobalVariables.backgroundColor,
+        iconSize: 28,
+        onTap: updatePage,
+        items: [
+          // HOME
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 0
+                        ? GlobalVariables.selectedNavBarColor
+                        : GlobalVariables.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.home_outlined,
+              ),
+            ),
+            label: '',
+          ),
+          // ACCOUNT
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 1
+                        ? GlobalVariables.selectedNavBarColor
+                        : GlobalVariables.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: const Icon(
+                Icons.person_outline_outlined,
+              ),
+            ),
+            label: '',
+          ),
+          // CART
+          BottomNavigationBarItem(
+            icon: Container(
+              width: bottomBarWidth,
+              decoration: BoxDecoration(
+                border: Border(
+                  top: BorderSide(
+                    color: _page == 2
+                        ? GlobalVariables.selectedNavBarColor
+                        : GlobalVariables.backgroundColor,
+                    width: bottomBarBorderWidth,
+                  ),
+                ),
+              ),
+              child: badges.Badge(
+                position: badges.BadgePosition.topEnd(top: -10, end: -12),
+                showBadge: true,
+                ignorePointer: false,
+                onTap: () {
+                  // Handle tap action here
+                },
+                badgeContent: Icon(Icons.check, color: Colors.white, size: 10),
+                badgeAnimation: badges.BadgeAnimation.rotation(
+                  animationDuration: Duration(seconds: 1),
+                  colorChangeAnimationDuration: Duration(seconds: 1),
+                  loopAnimation: false,
+                  curve: Curves.fastOutSlowIn,
+                  colorChangeAnimationCurve: Curves.easeInCubic,
+                ),
+                badgeStyle: badges.BadgeStyle(
+                  shape: badges.BadgeShape.square,
+                  badgeColor: Colors.blue,
+                  padding: EdgeInsets.all(5),
+                  borderRadius: BorderRadius.circular(4),
+                  borderSide: BorderSide(color: Colors.white, width: 2),
+                  borderGradient: badges.BadgeGradient.linear(
+                    colors: [Colors.red, Colors.black],
+                  ),
+                  badgeGradient: badges.BadgeGradient.linear(
+                    colors: [Colors.blue, Colors.yellow],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  elevation: 0,
+                ),
+                child: const Icon(Icons.shopping_cart_outlined),
+              ),
+            ),
+            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ Frontend/amazon_clone/lib/features/admin/widgets/category_products_chart.dart
@@ -0,0 +1,26 @@
+// import 'package:amazon_clone/features/admin/models/sales.dart'; // Adjust path as per your project structure
+// import 'package:charts_flutter/flutter.dart' as charts;
+// import 'package:flutter/material.dart';
+//
+// class CategoryProductsChart extends StatelessWidget {
+//   final List<charts.Series<Sales, String>> seriesList;
+//
+//   const CategoryProductsChart({
+//     Key? key,
+//     required this.seriesList,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return charts.BarChart(
+//       seriesList,
+//       animate: true,
+//       // Optionally provide chart behaviors such as axis formatting, tooltips, etc.
+//       behaviors: [
+//         charts.SeriesLegend(),
+//         charts.SlidingViewport(),
+//         charts.PanBehavior(),
+//       ],
+//     );
+//   }
+// }
